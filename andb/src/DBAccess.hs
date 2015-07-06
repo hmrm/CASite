c9c2ebe2d7a8c3a924dc5bf2266d521571036840
Haney Maxwell <hmrm@Penemue.local>
2015-07-05 21:50:35 -0700

    Add database initialization logic

--- andb/src/DBAccess.hs
+++ andb/src/DBAccess.hs
@@ -1,10 +1,34 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module DBAccess
        ( initializeDB ) where
 
 import Config
-import Database.PostgreSQL.Simple
+import Control.Exception
+import Database.PostgreSQL.Simple hiding (connect)
 import Data.ByteString.Char8
+import Data.Int
 
+connect :: Config -> IO Connection
+connect = connectPostgreSQL . pack . postgresConnectString
+
+duplicateTableSqlState :: ByteString
+duplicateTableSqlState = "42P07"
+
+-- TODO: Add index generation
+-- TODO: Document
+-- TODO: Separate into separate functions
 initializeDB :: Config -> IO Connection
-initializeDB = connectPostgreSQL . pack . postgresConnectString
-    
+initializeDB conf = do
+  conn <- connect conf
+  catchJust handleExists (runQuery conn) ignoreError
+  return conn
+    where
+      handleExists (e :: SqlError)
+        | sqlState e == duplicateTableSqlState = Just ()
+        | otherwise                          = Nothing
+      runQuery conn = (execute_ conn " CREATE TABLE ids (         \
+                                     \   id   SERIAL PRIMARY KEY, \
+                                     \   name TEXT NOT NULL)      ")
+      ignoreError _ = return (-1)
